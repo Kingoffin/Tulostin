58c6121de0f1c4cdd6ed158f2edc7ceca3a10ec6
Kingoffin <kalle.a.peltonen@gmail.com>
2023-10-10 16:04:30 +0300

    tehty pohja laskimelle

---
+++ src/TulostinOhjelma.java
@@ -0,0 +1,18 @@
+
+public class TulostinOhjelma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double luku1 = 7;
+		
+		double luku2 = 9;
+		
+		double tulo = luku1 * luku2;
+		
+		double erotus = luku1 - luku2;
+		
+		double summa = luku1 + luku2;
+		
+		double jako = luku1 / luku2;
+	
